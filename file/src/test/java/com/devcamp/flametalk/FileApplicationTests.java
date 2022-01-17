0151ab4abe72db4f330d1190541f954c4b83b550
SuyeonChoi <tndus970120@gmail.com>
2022-01-17 13:25:08 +0900

    [#7] chore: 파일 서버 초기 셋팅

---
+++ file/src/test/java/com/devcamp/flametalk/FileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcamp.flametalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
