6232ab992d95c9cfb360bbbf73de55f7cb3298d0
vo0a <vo0a195@gmail.com>
2022-01-25 15:17:05 +0900

    [#12] feat: 공통 모듈에 response 추가

---
+++ common/src/main/java/com/devcamp/flametalk/common/error/CustomException.java
@@ -0,0 +1,14 @@
+package com.devcamp.flametalk.common.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 기본적으로 제공되는 Exception 외에 사용하는 클래스입니다.
+ */
+@AllArgsConstructor
+@Getter
+public class CustomException extends RuntimeException {
+
+  private final ErrorCode errorCode;
+}
