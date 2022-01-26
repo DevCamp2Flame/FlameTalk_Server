33b53088176d50f344791c46c5a62f8a24891f38
SuyeonChoi <tndus970120@gmail.com>
2022-01-26 23:05:08 +0900

    [#3] feat: 예외처리

--- membership/src/main/java/com/devcamp/flametalk/domain/profile/service/ProfileService.java
+++ membership/src/main/java/com/devcamp/flametalk/domain/profile/service/ProfileService.java
@@ -4,9 +4,10 @@ import com.devcamp.flametalk.domain.profile.domain.Profile;
 import com.devcamp.flametalk.domain.profile.domain.ProfileRepository;
 import com.devcamp.flametalk.domain.profile.dto.ProfileDetailResponse;
 import com.devcamp.flametalk.domain.profile.dto.ProfileRequest;
+import com.devcamp.flametalk.domain.profile.error.ErrorCode;
+import com.devcamp.flametalk.domain.profile.error.exception.EntityNotFoundException;
 import com.devcamp.flametalk.domain.user.domain.User;
 import com.devcamp.flametalk.domain.user.domain.UserRepository;
-import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,7 +22,7 @@ public class ProfileService {
   @Transactional
   public Long save(ProfileRequest request) {
     User user = userRepository.findById(request.getUserId())
-        .orElseThrow(() -> new EntityNotFoundException("유저가 존재하지 않습니다."));
+        .orElseThrow(() -> new EntityNotFoundException(ErrorCode.USER_NOT_FOUND));
 
     Profile profile = request.toProfile(user);
     profileRepository.save(profile);
@@ -30,16 +31,16 @@ public class ProfileService {
 
   public ProfileDetailResponse findProfile(Long id) {
     Profile profile = profileRepository.findById(id)
-        .orElseThrow(() -> new EntityNotFoundException("프로필이 존재하지 않습니다."));
+        .orElseThrow(() -> new EntityNotFoundException(ErrorCode.PROFILE_NOT_FOUND));
     return ProfileDetailResponse.from(profile);
   }
 
   @Transactional
   public Long updateProfile(Long profileId, ProfileRequest request) {
     User user = userRepository.findById(request.getUserId())
-        .orElseThrow(() -> new EntityNotFoundException("유저가 존재하지 않습니다."));
+        .orElseThrow(() -> new EntityNotFoundException(ErrorCode.USER_NOT_FOUND));
     Profile profile = profileRepository.findById(profileId)
-        .orElseThrow(() -> new EntityNotFoundException("프로필이 존재하지 않습니다."));
+        .orElseThrow(() -> new EntityNotFoundException(ErrorCode.PROFILE_NOT_FOUND));
 
     Profile requestProfile = request.toProfile(user);
     Profile updatedProfile = profile.update(requestProfile);
