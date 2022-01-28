a76a8784e1549fa861c15d4170b78cefde4b0570
SuyeonChoi <tndus970120@naver.com>
2022-01-28 11:36:13 +0900

    [#3] membership 서버 프로필 API 구현 (#20)
    * feat: BaseTime 엔티티 구현
    
    * feat: 도메인 구현
    - Profile 엔티티 구현
    - File 엔티티 구현
    - User 엔티티 구현
    - 모든 도메인의 Repository 구현
    - 연관관계 매핑
    
    * [#3] feat: 프로필 등록 기능 구현
    
    * [#3] feat: 프로필 불러오기
    
    * [#3] feat: 프로필 수정하기
    
    * [#3] style: Google CheckStyle 적용
    - fix: annotation 수정(PatchMapping -> PutMapping)
    - fix: Transactional annotation 추가
    - style: model 패키지 위치변경
    
    * [#3] feat: domain, dto 업데이트
    - DB 테이블 변경에 따라 업데이트
    
    * [#3] refactor: 변경된 domain에 따라 API 업데이트
    - 응답 방식 변경
    
    * [#3] feat: 예외처리
    
    * [#3] docs: Javadoc 작성
    
    * [#3] style: EOF 해결
    
    * [#3] fix: 프로필 boolean 값 식별 문제 해결
    
    * [#3] feat: 스티커가 JSON Array 형태로 저장될 수 있도록 변경

---
+++ membership/src/main/java/com/devcamp/flametalk/domain/profile/controller/ProfileController.java
@@ -0,0 +1,78 @@
+package com.devcamp.flametalk.domain.profile.controller;
+
+import com.devcamp.flametalk.domain.profile.domain.ProfileResponse;
+import com.devcamp.flametalk.domain.profile.dto.CommonResponse;
+import com.devcamp.flametalk.domain.profile.dto.ProfileDetailResponse;
+import com.devcamp.flametalk.domain.profile.dto.ProfileRequest;
+import com.devcamp.flametalk.domain.profile.dto.SingleDataResponse;
+import com.devcamp.flametalk.domain.profile.service.ProfileService;
+import java.net.URI;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Membership 서버의 Profile API 처리 컨트롤러입니다.
+ */
+@Slf4j
+@CrossOrigin("*")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/profile")
+public class ProfileController {
+
+  private final ProfileService profileService;
+
+  /**
+   * 유저 본인의 프로필을 수정합니다.
+   *
+   * @param request 등록할 프로필 JSON 데이터
+   * @return 성공한 경우 DB에 저장된 id로 uri 생성
+   */
+  @PostMapping("/me")
+  public ResponseEntity<CommonResponse> create(@RequestBody @Valid ProfileRequest request) {
+    Long savedId = profileService.save(request);
+    log.info("create " + request.getUserId() + " user profile:" + savedId);
+    return ResponseEntity.created(URI.create("/api/profile/" + savedId))
+        .body(CommonResponse.from(ProfileResponse.PROFILE_CREATE_SUCCESS));
+  }
+
+  /**
+   * 특정 프로필을 조회합니다.
+   *
+   * @param profileId 조회할 프로필 id
+   * @return 프로필 상세 정보
+   */
+  @GetMapping("/{profileId}")
+  public ResponseEntity<SingleDataResponse> findProfile(@PathVariable Long profileId) {
+    ProfileDetailResponse profileDetail = profileService.findProfile(profileId);
+    SingleDataResponse<ProfileDetailResponse> response = new SingleDataResponse(
+        ProfileResponse.PROFILE_DETAIL_SUCCESS, profileDetail);
+    log.info("read profile" + response.getData().toString());
+    return ResponseEntity.ok().body(response);
+  }
+
+  /**
+   * 유저 본인의 프로필을 수정합니다.
+   *
+   * @param profileId 수정할 프로필 id
+   * @param request   수정될 프로필 JSON 데이터
+   * @return API 성공 여부 전달
+   */
+  @PutMapping("/me/{profileId}")
+  public ResponseEntity<CommonResponse> updateProfile(@PathVariable Long profileId,
+      @RequestBody @Valid ProfileRequest request) {
+    Long updatedId = profileService.updateProfile(profileId, request);
+    log.info("update " + request.getUserId() + " user profile:" + updatedId);
+    return ResponseEntity.ok().body(CommonResponse.from(ProfileResponse.PROFILE_UPDATE_SUCCESS));
+  }
+}
---
+++ membership/src/main/java/com/devcamp/flametalk/domain/user/domain/User.java
@@ -0,0 +1,55 @@
+package com.devcamp.flametalk.domain.user.domain;
+
+import com.devcamp.flametalk.domain.profile.domain.Profile;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * User 엔티티 입니다.
+ */
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+
+  @Id
+  private String id;
+
+  @NotNull
+  private String email;
+
+  private String password;
+
+  @NotNull
+  private String nickname;
+
+  @NotNull
+  private String phoneNumber;
+
+  @NotNull
+  private String birthday;
+
+  @NotNull
+  private short social;
+
+  @NotNull
+  private short status;
+
+  @NotNull
+  private boolean emailCheck;
+
+  @NotNull
+  private String region;
+
+  @NotNull
+  private String language;
+
+  @OneToMany(mappedBy = "user")
+  private List<Profile> profiles = new ArrayList<>();
+}
