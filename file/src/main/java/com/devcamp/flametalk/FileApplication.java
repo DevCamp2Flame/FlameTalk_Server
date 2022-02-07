4cbd84708645a180ddeb60f0ac4452b790aa1e14
SuyeonChoi <tndus970120@gmail.com>
2022-02-07 23:19:16 +0900

    [#51] chore: API gateway 구현

--- file/src/main/java/com/devcamp/flametalk/FileApplication.java
+++ file/src/main/java/com/devcamp/flametalk/FileApplication.java
@@ -2,11 +2,13 @@ package com.devcamp.flametalk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  * File 서버를 작동시키는 Application 클래스입니다.
  */
+@EnableDiscoveryClient
 @EnableJpaAuditing
 @SpringBootApplication
 public class FileApplication {
