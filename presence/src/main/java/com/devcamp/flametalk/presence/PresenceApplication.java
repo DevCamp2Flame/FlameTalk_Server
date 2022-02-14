b331580c1e66677772e5534c4b59504caae98665
vo0a <vo0a195@gmail.com>
2022-02-14 17:07:56 +0900

    [#90] chore: 채팅 서비스 API Gateway 적용
    - chat, presence 서버 eureka client 설정 (8085, 8086)
    - gateway 서버 yml 수정
    - 통신 테스트

--- presence/src/main/java/com/devcamp/flametalk/presence/PresenceApplication.java
+++ presence/src/main/java/com/devcamp/flametalk/presence/PresenceApplication.java
@@ -2,7 +2,9 @@ package com.devcamp.flametalk.presence;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PresenceApplication {
 
