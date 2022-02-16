b846aafc21a9b947eb0fe1dd06807bddb1d80f54
vo0a <vo0a195@gmail.com>
2022-02-16 16:48:15 +0900

    [#101] fix: FCM 서버 요청의 body 객체 수정

--- chat/src/main/java/com/devcamp/flametalk/service/Receiver.java
+++ chat/src/main/java/com/devcamp/flametalk/service/Receiver.java
@@ -1,19 +1,20 @@
 package com.devcamp.flametalk.service;
 
 import com.devcamp.flametalk.domain.Message;
-import com.devcamp.flametalk.dto.UserList;
-
-import java.util.Arrays;
+import com.devcamp.flametalk.dto.PushMessageRequest;
 import java.util.List;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -40,39 +41,54 @@ public class Receiver {
 
     // 채팅방 전체 사용자, 채팅방에 접속 상태인 사용자
     List<String> chatroomUser = getChatroomUsers(message.getRoom_id());
-    System.out.println(chatroomUser.get(0).toString());
+    log.info("chatroomUser get 0 : {}", chatroomUser.get(0));
     List<String> presenceUser = getInsideUsers(message.getRoom_id());
-    System.out.println(presenceUser.get(0).toString());
+    log.info("presenceUser get 0 : {}", presenceUser.get(0));
+
     // '전체 사용자 - 접속 상태인 사용자' 에 해당하는 사람들에게 FCM 메시지 전송
     chatroomUser.removeAll(presenceUser);
-    HttpHeaders headers = new HttpHeaders();
- //   headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-    headers.setContentType(MediaType.APPLICATION_JSON);
-    HttpEntity<Object> entity = new HttpEntity<>(chatroomUser,headers);
-    ResponseEntity<String> response =  restTemplate.exchange("http://localhost:8087/send", HttpMethod.GET,entity, String.class);
-    System.out.println(response.getBody());
+    sendFcmPushMessage(chatroomUser, message);
+
     // /chat/room/{roomId} 를 구독 하고 있는 사람들에게 메시지 전송
     template.convertAndSend("/sub/chat/room/" + message.getRoom_id(), message);
   }
 
-  private List<String> getChatroomUsers(String roomId) {
-//    ResponseEntity<UserList> response = restTemplate.getForEntity(
-//        "http://localhost:8084/api/chatroom/" + roomId + "/users", UserList.class);
-//    System.out.println(response.getBody());
-//    return Objects.requireNonNull(response.getBody()).getUserIds();
+  private void sendFcmPushMessage(List<String> chatroomUser, Message message) {
+    // http request 생성
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
 
+    // request body 에 fcm-server 에서 요구하는 request 형식에 맞아야 함
+    PushMessageRequest request = PushMessageRequest.builder()
+        .user_list(chatroomUser)
+        .message_id(message.getMessage_id())
+        .room_id(message.getRoom_id())
+        .sender_id(message.getSender_id())
+        .nickname(message.getNickname())
+        .contents(message.getContents())
+        .file_url(message.getFile_url())
+        .build();
+
+    HttpEntity<Object> entity = new HttpEntity<>(request, headers);
+    ResponseEntity<String> response = restTemplate.exchange("http://localhost:8087/send",
+        HttpMethod.GET, entity, String.class);
+
+    log.info("FCM response get Body : {}", response.getBody());
+  }
+
+  private List<String> getChatroomUsers(String roomId) {
     ResponseEntity<List<String>> response = restTemplate.exchange(
-            "http://localhost:8084/api/chatroom/" + roomId + "/users", HttpMethod.GET, null, new ParameterizedTypeReference<List<String>>() {
-            });
-    List<String> list = response.getBody();
-    return list;
+        "http://localhost:8084/api/chatroom/" + roomId + "/users", HttpMethod.GET, null,
+        new ParameterizedTypeReference<>() {
+        });
+    return Objects.requireNonNull(response.getBody());
   }
 
   private List<String> getInsideUsers(String roomId) {
-
     ResponseEntity<List<String>> response = restTemplate.exchange(
-        "http://localhost:8086/api/presence/" + roomId,  HttpMethod.GET, null, new ParameterizedTypeReference<List<String>>(){});
-    List<String> list = response.getBody();
-    return list;
+        "http://localhost:8086/api/presence/" + roomId, HttpMethod.GET, null,
+        new ParameterizedTypeReference<>() {
+        });
+    return Objects.requireNonNull(response.getBody());
   }
 }
